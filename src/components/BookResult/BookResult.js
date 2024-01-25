9d6c667bac7b43f27447a7f3103628a047087f96
Michael Snowdon <151382049+michaelesnowdon@users.noreply.github.com>
2024-01-25 13:35:24 +0000

    add add a book to reading list functionality for user

--- src/components/BookResult/BookResult.js
+++ src/components/BookResult/BookResult.js
@@ -1,7 +1,10 @@
 import "./BookResult.scss";
 import ImageNotFound from "../../assets/images/image-not-found-icon.svg";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+import axios from "axios";
 
 function BookResult({ book }) {
+  const { user, getToken } = useKindeAuth();
   // Extract book details to variables
   const { title, authors, categories, description, imageLinks, pageCount } =
     book;
@@ -13,6 +16,7 @@ function BookResult({ book }) {
       (identifier) => identifier.type === "ISBN_13"
     );
 
+  const userId = user.id;
   const postIsbn = isbn13.identifier;
   const postTitle = title;
   const postAuthor = authors[0];
@@ -22,13 +26,39 @@ function BookResult({ book }) {
     imageLinks && imageLinks.thumbnail ? imageLinks.thumbnail : "";
   const postPageCount = pageCount;
 
-  // console.log(postIsbn);
-  // console.log(postTitle);
-  // console.log(postAuthor);
-  // console.log(postCategory);
-  // console.log(postDesc);
-  // console.log(postThumbnail);
-  // console.log(postPageCount);
+  const AddABookToReadList = async () => {
+    try {
+      const accessToken = await getToken();
+      const response = await axios.post(
+        `http://localhost:8080/api/books`,
+        {
+          userId: userId,
+          bookId: postIsbn,
+          title: postTitle,
+          author: postAuthor,
+          category: postCategory,
+          description: postDesc,
+          thumbnail: postThumbnail,
+          pageCount: postPageCount,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleAddBook = async () => {
+    try {
+      await AddABookToReadList();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <article className="bookresult">
@@ -49,7 +79,9 @@ function BookResult({ book }) {
             Category: {categories ? categories.join(", ") : "N/A"}
           </p>
           <p className="bookresult__description">{description || "N/A"}</p>
-          <button className="bookresult__button">Add to Reading List</button>
+          <button className="bookresult__button" onClick={handleAddBook}>
+            Add to Reading List
+          </button>
         </div>
       </div>
     </article>
